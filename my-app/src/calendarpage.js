d6152116c48f98e265d081662a1c1045aeaf4273
Kasvi123 <singh.kasvi.com>
2024-09-21 04:50:12 -0400

    updated features of css and js of calendar

--- my-app/src/calendarpage.js
+++ my-app/src/calendarpage.js
@@ -16,10 +16,14 @@ export default function CoursePage() {
       </nav>
 
       {/* Google Calendar Embed */}
-      <div className="content">
-        <iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=America%2FNew_York&bgcolor=%23ffffff&src=c2luZ2gua2FzdmlAZ21haWwuY29t&src=ZjllMGVlZTdjNWVkOGZmY2MyYmUyM2Q5YmM0YTgzYjg3MTI0ZTM5YjNkNmNhYzI2NzMwN2FkMWY3N2Q5ZjgwOUBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&src=Y19hYzdkZWU3NjVhZDQ0ODNjNmFjNzRiOGRiNTRiNTk3NDI3MTVlOTQ0MzY4OTJkMTkzMzhkMTk1MjY4M2RjMWQyQGdyb3VwLmNhbGVuZGFyLmdvb2dsZS5jb20&color=%239E69AF&color=%23F6BF26&color=%237986CB" 
-        scrolling="yes">
-        </iframe>
+      <div className="calendar-container">
+        <iframe 
+          src="https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=America%2FNew_York&bgcolor=%23ffffff&src=c2luZ2gua2FzdmlAZ21haWwuY29t&src=ZjllMGVlZTdjNWVkOGZmY2MyYmUyM2Q5YmM0YTgzYjg3MTI0ZTM5YjNkNmNhYzI2NzMwN2FkMWY3N2Q5ZjgwOUBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&src=Y19hYzdkZWU3NjVhZDQ0ODNjNmFjNzRiOGRiNTRiNTk3NDI3MTVlOTQ0MzY4OTJkMTkzMzhkMTk1MjY4M2RjMWQyQGdyb3VwLmNhbGVuZGFyLmdvb2dsZS5jb20&color=%239E69AF&color=%23F6BF26&color=%237986CB" 
+          style={{ border: 0, width: '100%', height: '600px' }} 
+          frameBorder="0" 
+          scrolling="no" 
+          title="Google Calendar"
+        ></iframe>
       </div>
     </div>
   );
