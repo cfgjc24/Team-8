ebdc8a5a56f13ed918440186aa1c08dde462ed10
Benita Besa <benita.besa@duke.edu>
2024-09-20 21:36:51 -0400

    Added routes to login & sign up forms

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,11 @@
-import logo from './FGN-removebg-preview.png';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import logo from './FGN-removebg-preview.png';  // Your logo
+import './App.css';  // Your CSS file
+import LoginPage from './LoginPage';  // Login Page component
+import SignUpForm from './SignUpForm';  // Registration form component (SignUpForm)
 
-
-function App() {
+// Home Page Component
+function HomePage() {
   return (
     <div className="App">
       <header className="App-header">
@@ -11,18 +14,45 @@ function App() {
           <p>Please choose your role</p>
         </div>
         <div className="button-container">
-          <button className="App-button">Student</button>
-          <button className="App-button">Tutor</button>
-          <button className="App-button">Admin</button>
+          <Link to="/login" className="App-button">
+            <button className="App-button">Student</button>
+          </Link>
+          <Link to="/login" className="App-button">
+            <button className="App-button">Tutor</button>
+          </Link>
+          <Link to="/login" className="App-button">
+            <button className="App-button">Admin</button>
+          </Link>
         </div>
         <div className="App-footer">
-          <button className="button">Don't have an account? Sign up</button>
+          <Link to="/register">
+            <button className="button">Don't have an account? Sign up</button>
+          </Link>
         </div>
       </header>
     </div>
   );
 }
 
+// Main App Component with Routing
+function App() {
+  return (
+    <Router>
+      <Routes>
+        {/* Route for the homepage */}
+        <Route path="/" element={<HomePage />} />
+
+        {/* Route for the login page */}
+        <Route path="/login" element={<LoginPage />} />
+
+        {/* Route for the registration page */}
+        <Route path="/register" element={<SignUpForm />} />
+      </Routes>
+    </Router>
+  );
+}
+
 export default App;
+
 
 
