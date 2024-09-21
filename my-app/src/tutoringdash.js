f74482e68f06e61874fbd54b25ae3f87ce08b4e0
DanielA <dmalemayehu15@gmail.com>
2024-09-21 03:04:40 -0400

    Created tutoring dashboard

---
+++ my-app/src/tutoringdash.js
@@ -0,0 +1,49 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+function tutors() {
+  return (
+    
+    <div id="projects">
+        <div class="container">
+            <h1 class="sub-title">Tutor Dashboard</h1>
+            <div class="work-list">
+                <div class="work">
+                  
+                    <div class="layer">
+                        <h3>Documents</h3>
+                        <p></p>
+                        <a href="https://www.firstgenerationinvestors.com/enroll.html" target="_blank" rel="noopener noreferrer" className="button">
+                   FORMS INCOMPLETE
+              </a>
+                    </div>
+                </div>
+                <div class="work">
+                  
+                    <div class="layer">
+                        <h3>Tutor Info/Training</h3>
+                        <p></p>
+                        <a href="https://www.flipcause.com/secure/your_details_more_info/MjExMTMw/152981" target="_blank" rel="noopener noreferrer" className="button">
+TUTOR TRAINING
+              </a>
+                         </div>
+                </div>
+                <div class="work">
+                   
+                    <div class="layer">
+                        <h3>Your Students</h3>
+                        <p></p>
+                        <a href="hhttps://www.khanacademy.org/college-careers-more/financial-literacy" target="_blank" rel="noopener noreferrer" className="button">
+               STUDENTS VIEW
+              </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+    </div>
+  );
+}
+
+export default tutors;
---
+++ my-app/src/tutors.js
@@ -0,0 +1,48 @@
+import logo from './logo.svg';
+import './tutorcss.css';
+
+
+function tutors() {
+  return (
+    
+    <div id="projects">
+        <div class="container">
+            <h1 class="sub-title">Oppertunities</h1>
+            <div class="work-list">
+                <div class="work">
+                  
+                    <div class="layer">
+                        <h3>Clubs</h3>
+                        <p>Finance Related clubs</p>
+                        <a href="https://www.charitycharge.com/nonprofit-resources/financial-literacy-nonprofits/" target="_blank" rel="noopener noreferrer" className="button">
+                  Club Oppertunities
+              </a>
+                    </div>
+                </div>
+                <div class="work">
+                  
+                    <div class="layer">
+                        <h3>Internships</h3>
+                        <p>Finance related Internships</p>
+                        <a href="https://www.ziprecruiter.com/Jobs/High-School-Finance-Intern/-in-New-York,NY" target="_blank" rel="noopener noreferrer" className="button">
+Internships Oppertunities
+              </a>
+                         </div>
+                </div>
+                <div class="work">
+                   
+                    <div class="layer">
+                        <h3>Additional Courses </h3>
+                        <p>Finance Related courses</p>
+                        <a href="hhttps://www.khanacademy.org/college-careers-more/financial-literacy" target="_blank" rel="noopener noreferrer" className="button">
+               Meeting Oppertunities
+              </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  );
+}
+
+export default oppertunities;
