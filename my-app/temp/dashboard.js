2a805a4783427e11cc2a2f7b6219a37d27083664
DanielA <dmalemayehu15@gmail.com>
2024-09-20 23:32:30 -0400

    Updated interface for dashboard page

---
+++ my-app/temp/dashboard.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './dashboardcss.css';
+
+function App() {
+  return (
+    <header className = "header">
+        <a href="/" className = "home">Home</a>
+        <nav className = "navbar">
+            <a className = "courselink" href = "courses.html">Courses</a>
+            <a className = "calendarlink" href = "calendar.html">Calendar</a>
+            <a className = "opportunitylink" href = "opportunities.html">Opportunities</a>
+        </nav>
+    </header>
+  );
+}
+
+export default App;
