38740a1ba875ecec96df024f9932d40576c29a75
Joshua Brown <joshuabrownics@gmail.com>
2024-09-20 22:35:25 -0400

    Add Tutor model with relationship to Student

--- backend/models.py
+++ backend/models.py
@@ -16,8 +16,8 @@ class Student(Base):
     tutor = relationship('Tutor', back_populates='students')
     posts = relationship('Post', back_populates='student', cascade='all, delete-orphan')
     comments = relationship('Comment', back_populates='student', cascade='all, delete-orphan')  
-    
-    class Tutor(Base):
+
+class Tutor(Base):
     __tablename__ = 'tutors'
     id = Column(Integer, primary_key=True, autoincrement=True)
     first_name = Column(String, nullable=False)
@@ -25,3 +25,13 @@ class Student(Base):
     age = Column(Integer, nullable=False)
     email = Column(String, nullable=False, unique=True)
     students = relationship('Student', back_populates='tutor')
+    
+class Post(Base):
+    __tablename__ = 'posts'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    student_id = Column(Integer, ForeignKey('students.id', ondelete='CASCADE'))
+    content = Column(String, nullable=False)
+    created_at = Column(DateTime, nullable=False, server_default='CURRENT_TIMESTAMP')
+    student = relationship('Student', back_populates='posts')
+    comments = relationship('Comment', back_populates='post', cascade='all, delete-orphan')
+
