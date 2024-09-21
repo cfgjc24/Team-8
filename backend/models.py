d8123054edd103085c83536958a8720b95da55e1
Joshua Brown <joshuabrownics@gmail.com>
2024-09-20 22:32:44 -0400

    Create Student model with relationships to Tutor, Post, and Comment

--- backend/models.py
+++ backend/models.py
@@ -1,23 +1,27 @@
-from sqlalchemy.ext.declarative import declarative_base 
+from sqlalchemy import create_engine, Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, sessionmaker
+
+Base = declarative_base()
 
 class Student(Base):
     __tablename__ = 'students'
-    
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     first_name = Column(String, nullable=False)
     last_name = Column(String, nullable=False)
     age = Column(Integer, nullable=False)
     email = Column(String, nullable=False, unique=True)
     dob = Column(DateTime, nullable=False)
-
-class Tutor(Base):
-    __tablename__ = 'students'
+    tutor_id = Column(Integer, ForeignKey('tutors.id', ondelete='SET NULL'))
+    tutor = relationship('Tutor', back_populates='students')
+    posts = relationship('Post', back_populates='student', cascade='all, delete-orphan')
+    comments = relationship('Comment', back_populates='student', cascade='all, delete-orphan')  
     
-    id = Column(Integer, primary_key=True)
+    class Tutor(Base):
+    __tablename__ = 'tutors'
+    id = Column(Integer, primary_key=True, autoincrement=True)
     first_name = Column(String, nullable=False)
     last_name = Column(String, nullable=False)
     age = Column(Integer, nullable=False)
     email = Column(String, nullable=False, unique=True)
-    dob = Column(DateTime, nullable=False)
-
-
+    students = relationship('Student', back_populates='tutor')
