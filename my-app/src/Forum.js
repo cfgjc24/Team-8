d270d68647ff3f1863b224d49c4cb238d1805cd1
DanielA <dmalemayehu15@gmail.com>
2024-09-21 09:56:28 -0400

    Link profile page to other pages in site

--- my-app/src/Forum.js
+++ my-app/src/Forum.js
@@ -64,7 +64,7 @@ const Forum = () => {
           <li><Link to="/courses" className="nav-item">Courses</Link></li>
           <li><Link to="/calendar" className="nav-item">Calendar</Link></li>
           <li><Link to="/opportunities" className="nav-item">Opportunities</Link></li>
-          <li><Link to="/profile" className="nav-item">Profile</Link></li>
+          <li><Link to="/profileupdate" className="nav-item">Profile</Link></li>
         </ul>
       </nav>
 
